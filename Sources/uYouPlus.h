9e8526da8b1b7aead8a3184b88daa76148d13425
aric3435 <157071384+arichornloverALT@users.noreply.github.com>
2024-10-09 15:53:59 -0500

    Update uYouPlus.h

--- Sources/uYouPlus.h
+++ Sources/uYouPlus.h
@@ -259,12 +259,6 @@ static NSString *const kGoogleSigninFix = @"googleSigninFix_enabled";
 - (void)confirmAlertDidPressConfirm;
 @end
 
-@interface ELMPBShowActionSheetCommand : GPBMessage // for uYouPlusPatches.xm
-@property (nonatomic, strong, readwrite) ELMPBCommand *onAppear;
-@property (nonatomic, assign, readwrite) BOOL hasOnAppear;
-- (id)listOptionArray;
-@end
-
 @interface YTTransportControlsButtonView : UIView
 @end
 
